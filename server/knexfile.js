82fe75c05cb72e20e874c7b14923b9bf6d5befaf
JohnG585 <jgallagher585@gmail.com>
2017-05-17 18:49:58 -0600

    imported folders from pt2

---
+++ server/knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || 'classifieds'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || 'classifieds'
+    }},
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+    },
+
+};
